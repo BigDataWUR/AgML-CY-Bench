becb7b8589a2efb79dc6ae26d668f69f1f3e6656
krsnapaudel <dilli.paudel@wur.nl>
2024-01-03 10:59:33 +0100

    data_sources provide details of different data

--- datasets/crop_yield_dataset.py
+++ datasets/crop_yield_dataset.py
@@ -6,12 +6,8 @@ from datasets.dataset import AgMLBaseDataset
 
 class CropYieldDataset(AgMLBaseDataset):
 
-    YEARS_TRAIN = [2000, 2001]  # TODO -- define
-    YEARS_TEST = [2002]
-
-    INDEX_NAME = ('COUNTY_ID', 'FYEAR')
-
-    def __init__(self, data_dfs=None, data_path=None, data_sources=None):
+    def __init__(self, data_sources, data_dfs=None, data_path=None):
+        assert ((data_dfs is not None) or (data_path is not None))
         # Load the data from disk if necessary
         if (data_dfs is None):
             data_dfs = {}
@@ -21,179 +17,105 @@ class CropYieldDataset(AgMLBaseDataset):
                 src_df = CropYieldDataset.get_data(data_path, filename, index_cols)
                 data_dfs[src] = src_df
 
-        df_yield = data_dfs["YIELD"]
-        df_meteo = data_dfs["METEO"]
-        df_soil = data_dfs["SOIL"]
-        df_remote_sensing = data_dfs["REMOTE_SENSING"]
-
         # TODO -- align data
         # For now, only use counties that are present everywhere
-        counties_yield = set(df_yield.index.get_level_values(0))
-        counties_soil = set(df_soil.index.values)
-        counties_meteo = set(df_meteo.index.get_level_values(0))
-        counties_rs = set(df_remote_sensing.index.get_level_values(0))
-        counties = set.intersection(counties_yield, counties_soil, counties_meteo, counties_rs)
-
-        df_yield = CropYieldDataset._filter_df_on_index(df_yield, list(counties), level=0)
-        df_soil = CropYieldDataset._filter_df_on_index(df_soil, list(counties), level=0)
-        df_meteo = CropYieldDataset._filter_df_on_index(df_meteo, list(counties), level=0)
-        df_remote_sensing = CropYieldDataset._filter_df_on_index(df_remote_sensing, list(counties), level=0)
-
         # TODO -- data preprocessing:
         #   - Start date, end date of season?
         #   - Missing data?
 
-        self._data_y = df_yield  # Filter from raw data based on inputs/selection
+        spatial_units = None
+        for src in data_dfs:
+            src_units = set(data_dfs[src].index.get_level_values(0))
+            if (spatial_units is None):
+                spatial_units = src_units
+            else:
+                spatial_units = set.intersection(spatial_units, src_units)
 
-        self._data_soil = df_soil
-        self._data_meteo = df_meteo
-        self._data_remote_sensing = df_remote_sensing
+        for src in data_dfs:
+            src_df = CropYieldDataset._filter_df_on_index(data_dfs[src],
+                                                          list(spatial_units),
+                                                          level=0)
+            # Sort the data for faster lookups
+            src_df.sort_index(inplace=True)
+            data_dfs[src] = src_df
 
-        # Sort the data for faster lookups
-        self._data_y.sort_index(inplace=True)
-        self._data_soil.sort_index(inplace=True)
-        self._data_meteo.sort_index(inplace=True)
-        self._data_remote_sensing.sort_index(inplace=True)
-
-    @property
-    def years(self) -> list:
-        """
-        Obtain a list containing all years occurring in the dataset
-        """
-        return list(set([year for _, year in self._data_y.index.values]))
-
-    @property
-    def county_ids(self) -> list:
-        """
-        Obtain a list containing all county ids occurring in the dataset
-        """
-        return list(set([loc for loc, _ in self._data_y.index.values]))
-
-    @staticmethod
-    def target_key() -> str:
-        return 'YIELD'
-
-    @staticmethod
-    def feature_keys() -> tuple:
-        return CropYieldDataset.feature_keys_meteo() + CropYieldDataset.feature_keys_soil() + CropYieldDataset.feature_keys_remote_sensing()
-
-    @staticmethod
-    def feature_keys_meteo() -> tuple:
-        return 'TMAX', 'TMIN', 'TAVG', 'VPRES', 'WSPD', 'PREC', 'ET0', 'RAD'
-
-    @staticmethod
-    def feature_keys_soil() -> tuple:
-        return 'SM_WHC', 'SM_DEPTH'
-
-    @staticmethod
-    def feature_keys_remote_sensing() -> tuple:
-        return 'FAPAR',
+        self._data_y = data_dfs["YIELD"]
+        self._data_sources = data_sources
+        self._data_dfs = data_dfs
 
     def __getitem__(self, index) -> dict:
         # Index is either integer or tuple of (year, location)
 
         if isinstance(index, int):
             sample_y = self._data_y.iloc[index]
-            county_id, year = sample_y.name
+            spatial_unit, year = sample_y.name
 
         elif isinstance(index, tuple):
-            county_id, year = index
+            spatial_unit, year = index
             sample_y = self._data_y.loc[index]
 
         else:
             raise Exception(f'Unsupported index type {type(index)}')
 
-        data_meteo = self._get_meteo_data(county_id, year)
-        data_soil = self._get_soil_data(county_id)
-        data_remote_sensing = self._get_remote_sensing_data(county_id, year)
-
-        return {
+        data_item = {
             'FYEAR': year,
-            'COUNTY_ID': county_id,
+            'COUNTY_ID': spatial_unit,
             'YIELD': sample_y.YIELD,
-            **data_meteo,
-            **data_soil,
-            **data_remote_sensing,
         }
 
-    def _get_meteo_data(self, county_id: str, year: int) -> dict:
+        for src in self._data_sources:
+            if (src == "YIELD"):
+                continue
 
-        # Select data matching the location and year
-        # Sort the index for improved lookup speed
-        df = self._data_meteo.xs((county_id, year), drop_level=True)
+            if ("FYEAR" in self._data_sources[src]["index_cols"]):
+                src_sample = self._get_data_sample(src, spatial_unit, year)
+            else:
+                src_sample = self._get_data_sample(src, spatial_unit)
+            
+            data_item.update(**src_sample)
 
-        # Return the data as dict mapping
-        #  key -> np.ndarray
-        #  where the array contains data for all DEKADs
-        return {
-            key: df[key].values for key in self.feature_keys_meteo()
-        }
+        return data_item
 
-    def _get_soil_data(self, county_id: str) -> dict:
+    def _get_data_sample(self, src, spatial_unit, year=None):
 
-        # Select the data matching the location
-        df = self._data_soil.loc[county_id]
+        sel_cols = self._data_sources[src]["sel_cols"]
 
-        return {
-            key: df[key] for key in self.feature_keys_soil()
-        }
+        # static data, no year
+        if (year is None):
+            df = self._data_dfs[src].loc[spatial_unit]
+            sample = {
+                key : df[key] for key in sel_cols
+            }
 
-    def _get_remote_sensing_data(self, county_id: str, year: int) -> dict:
-        # Select data matching the location and year
-        # Sort the index for improved lookup speed
-        df = self._data_remote_sensing.xs((county_id, year), drop_level=True)
+        # time series data
+        else:
+            df = self._data_dfs[src].xs((spatial_unit, year), drop_level=True)
+            sample = {
+                key : df[key].values for key in sel_cols
+            }
 
-        # Return the data as dict mapping
-        #  key -> np.ndarray
-        #  where the array contains data for all DEKADs
-        return {
-            key: df[key].values for key in self.feature_keys_remote_sensing()
-        }
+        return sample
 
     def __len__(self) -> int:
         return len(self._data_y)
 
     def split_on_years(self, years_split: tuple) -> tuple:
 
-        df_yield_1, df_yield_2 = self._split_df_on_index(self._data_y, years_split, level=1)
-        df_meteo_1, df_meteo_2 = self._split_df_on_index(self._data_meteo, years_split, level=1)
-        df_rs_1, df_rs_2 = self._split_df_on_index(self._data_remote_sensing, years_split, level=1)
+        data_dfs1 = {}
+        data_dfs2 = {}
+        for src in self._data_dfs:
+            src_df = self._data_dfs[src]
+            if ("FYEAR" in self._data_sources[src]["index_cols"]):
+                df1, df2 = self._split_df_on_index(src_df, years_split, level=1)
+                data_dfs1[src] = df1
+                data_dfs2[src] = df2
+            else:
+                data_dfs1[src] = src_df.copy()
+                data_dfs2[src] = src_df.copy()
 
         return (
-            CropYieldDataset(data_dfs={
-                "YIELD" : df_yield_1,
-                "METEO" : df_meteo_1,
-                "SOIL" : self._data_soil.copy(), # No split required
-                "REMOTE_SENSING" : df_rs_1,
-            }),
-            CropYieldDataset(data_dfs={
-                "YIELD" : df_yield_2,
-                "METEO" : df_meteo_2,
-                "SOIL" : self._data_soil.copy(), # No split required
-                "REMOTE_SENSING" : df_rs_2,
-            }),
-        )
-
-    def split_on_county_ids(self, county_ids_split: tuple) -> tuple:
-
-        df_yield_1, df_yield_2 = self._split_df_on_index(self._data_y, county_ids_split, level=0)
-        df_meteo_1, df_meteo_2 = self._split_df_on_index(self._data_meteo, county_ids_split, level=0)
-        df_soil_1, df_soil_2 = self._split_df_on_index(self._data_soil, county_ids_split, level=0)
-        df_rs_1, df_rs_2 = self._split_df_on_index(self._data_remote_sensing, county_ids_split, level=0)
-
-        return (
-            CropYieldDataset(data_dfs={
-                "YIELD" : df_yield_1,
-                "METEO" : df_meteo_1,
-                "SOIL" : df_soil_1,
-                "REMOTE_SENSING" : df_rs_1,
-            }),
-            CropYieldDataset(data_dfs={
-                "YIELD" : df_yield_2,
-                "METEO" : df_meteo_2,
-                "SOIL" : df_soil_2,
-                "REMOTE_SENSING" : df_rs_2,
-            }),
+            CropYieldDataset(self._data_sources, data_dfs=data_dfs1),
+            CropYieldDataset(self._data_sources, data_dfs=data_dfs2),
         )
 
     @staticmethod
@@ -222,13 +144,6 @@ class CropYieldDataset(AgMLBaseDataset):
                 [df.xs(key, level=level, drop_level=False) for key in keys]
             )
 
-    @staticmethod
-    def train_test_datasets(data_path, data_sources) -> tuple:  # TODO -- define the splits
-        dataset = CropYieldDataset(data_path=data_path, data_sources=data_sources)
-        return dataset.split_on_years(
-            years_split=(CropYieldDataset.YEARS_TRAIN, CropYieldDataset.YEARS_TEST),
-        )
-
 from config import PATH_DATA_DIR
 
 if __name__ == '__main__':
@@ -237,29 +152,29 @@ if __name__ == '__main__':
         "YIELD" : {
             "filename" : "YIELD_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID", "FYEAR"],
+            "sel_cols" : ["YIELD"]
         },
         "METEO" : {
             "filename" : "METEO_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID", "FYEAR", "DEKAD"],
+            "sel_cols" : ["TMAX", "TMIN", "TAVG", "PREC", "ET0", "RAD"]
         },
         "SOIL" : {
             "filename" : "SOIL_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID"],
+            "sel_cols" : ["SM_WHC"]
         },
         "REMOTE_SENSING" : {
             "filename" : "REMOTE_SENSING_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID", "FYEAR", "DEKAD"],
+            "sel_cols" : ["FAPAR"]
         }
     }
 
-    _dataset = CropYieldDataset(data_path=data_path, data_sources=data_sources)
+    _dataset = CropYieldDataset(data_sources, data_path=data_path)
 
     # print(_dataset.years)
     # print(_dataset.locations)
     print(_dataset['AL_LAWRENCE', 2000])
 
-    dataset_train, dataset_test = CropYieldDataset.train_test_datasets(data_path, data_sources)
-
-    print(dataset_train.years)
-
-    dataset_train, dataset_validation = dataset_train.split_on_years(([2000], [2001]))
+    dataset_train, dataset_test = _dataset.split_on_years(([2000], [2001]))
--- models/naive_models.py
+++ models/naive_models.py
@@ -4,27 +4,28 @@ import pickle
 from models.model import AgMLBaseModel
 
 class AverageYieldModel(AgMLBaseModel):
-    def __init__(self, group_cols, year_col, label_col):
-        self.averages = None
-        self.group_cols = group_cols
-        self.year_col = year_col
-        self.target_col = label_col
+
+    def __init__(self, group_cols, year_col, target_col):
+        self._averages = None
+        self._group_cols = group_cols
+        self._year_col = year_col
+        self._target_col = target_col
 
     def fit(self, train_dataset):
         train_df = self._data_to_pandas(train_dataset)
         # print(train_df.head(5))
-        self.averages = train_df.groupby(self.group_cols).agg(PREDICTION=(self.target_col, "mean")).reset_index()
+        self._averages = train_df.groupby(self._group_cols).agg(PREDICTION=(self._target_col, "mean")).reset_index()
         # print(self.averages.head(5))
 
     def predict(self, data):
         test_df = self._data_to_pandas(data)
-        test_preds = test_df.merge(self.averages, on=self.group_cols)
+        test_preds = test_df.merge(self._averages, on=self._group_cols)
     
         return test_preds
 
     def predict(self, test_dataset):
         test_df = self._data_to_pandas(test_dataset)
-        test_preds = test_df.merge(self.averages, on=self.group_cols)
+        test_preds = test_df.merge(self._averages, on=self._group_cols)
     
         return test_preds
 
@@ -32,11 +33,11 @@ class AverageYieldModel(AgMLBaseModel):
         data = []
         for i in range(len(dataset)):
             data_item = dataset[i]
-            sel_item = [data_item[c] for c in self.group_cols]
-            sel_item += [data_item[self.year_col], data_item[self.target_col]]
+            sel_item = [data_item[c] for c in self._group_cols]
+            sel_item += [data_item[self._year_col], data_item[self._target_col]]
             data.append(sel_item)
 
-        data_cols = self.group_cols + [self.year_col, self.target_col]
+        data_cols = self._group_cols + [self._year_col, self._target_col]
         return pd.DataFrame(data, columns=data_cols)
 
     def save(self, model_name):
@@ -55,6 +56,7 @@ import os
 
 from datasets.crop_yield_dataset import CropYieldDataset
 from config import PATH_DATA_DIR
+from config import PATH_OUTPUT_DIR
 
 if __name__ == '__main__':
     data_path = os.path.join(PATH_DATA_DIR, "data_US", "county_data")
@@ -62,34 +64,44 @@ if __name__ == '__main__':
         "YIELD" : {
             "filename" : "YIELD_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID", "FYEAR"],
+            "sel_cols" : ["YIELD"]
         },
         "METEO" : {
             "filename" : "METEO_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID", "FYEAR", "DEKAD"],
+            "sel_cols" : ["TMAX", "TMIN", "TAVG", "PREC", "ET0", "RAD"]
         },
         "SOIL" : {
             "filename" : "SOIL_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID"],
+            "sel_cols" : ["SM_WHC"]
         },
         "REMOTE_SENSING" : {
             "filename" : "REMOTE_SENSING_COUNTY_US.csv",
             "index_cols" : ["COUNTY_ID", "FYEAR", "DEKAD"],
+            "sel_cols" : ["FAPAR"]
         }
     }
 
     train_years = [y for y in range(2000, 2012)]
     test_years = [y for y in range(2012, 2019)]
-    dataset = CropYieldDataset(data_path=data_path, data_sources=data_sources)
+    dataset = CropYieldDataset(data_sources, data_path=data_path)
     train_dataset, test_dataset = dataset.split_on_years((train_years, test_years))
  
     average_model = AverageYieldModel(group_cols=["COUNTY_ID"],
                                       year_col="FYEAR",
-                                      label_col="YIELD")
+                                      target_col="YIELD")
     average_model.fit(train_dataset)
     test_preds = average_model.predict(test_dataset)
     print(test_preds.head(5).to_string())
 
-    # average_model.save("saved_average_model.pkl")
-    # saved_model = AverageYieldModel.load("saved_average_model.pkl")
-    # data = np.zeros((2, 2))
-    # print(saved_model.predict(data))
+    output_path = os.path.join(PATH_OUTPUT_DIR, "saved_models")
+    os.makedirs(output_path, exist_ok=True)
+
+    # Test saving and loading
+    average_model.save(output_path + "/saved_average_model.pkl")
+    saved_model = AverageYieldModel.load(output_path + "/saved_average_model.pkl")
+    test_preds = saved_model.predict(test_dataset)
+    print("\n")
+    print("Predictions of saved model. Should match earlier output.")
+    print(test_preds.head(5).to_string())
