122a458ade90c6144b8a06f2b1ce94c8e69c4c8c
krsnapaudel <dilli.paudel@wur.nl>
2024-02-23 12:33:13 +0100

    #58 move trend computation to a separate function

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -3,31 +3,27 @@ import pandas as pd
 import numpy as np
 
 from datasets.dataset import Dataset
-from models.naive_models import AverageYieldModel
+from models.trend_models import TrendModel
 from config import PATH_DATA_DIR
 
 
-def test_predict_item():
-    model = AverageYieldModel(group_cols=["COUNTY_ID"], label_col="YIELD")
-    data_sources = {
-        "YIELD": {
-            "index_cols": ["COUNTY_ID", "FYEAR"],
-            "sel_cols": ["YIELD"],
-        }
-    }
-
+def test_trend_model():
+    trend_window = 5
+    x_cols = ["FYEAR-" + str(i) for i in range(trend_window, 0, -1)]
+    y_cols = ["YIELD-" + str(i) for i in range(trend_window, 0, -1)]
+    model = TrendModel(x_cols, y_cols, "FYEAR")
     data_path = os.path.join(PATH_DATA_DIR, "data_US", "county_data")
     yield_csv = os.path.join(data_path, "YIELD_COUNTY_US.csv")
-    yield_df = pd.read_csv(yield_csv, index_col=data_sources["YIELD"]["index_cols"])
-    data_dfs = {"YIELD": yield_df}
-    dataset = Dataset(data_dfs, data_sources, "COUNTY_ID", "FYEAR")
-    filtered_df = yield_df[yield_df.index.get_level_values(0) == "AL_AUTAUGA"]
-    expected_pred = filtered_df["YIELD"].mean()
+    yield_df = pd.read_csv(yield_csv, index_col=["COUNTY_ID", "FYEAR"])
+    # TODO: Create trend features
+    trend_fts = None
+    dataset = Dataset(yield_df, feature_dfs=[trend_fts])
+    expected_pred = 8.2
     model.fit(dataset)
     test_data = {
         "COUNTY_ID": "AL_AUTAUGA",
         "FYEAR": 2018,
-        "YIELD": yield_df.loc[("AL_AUTAUGA", 2018)]["YIELD"],
+        # TODO: add trend years and features
     }
 
     test_preds, _ = model.predict_item(test_data)
