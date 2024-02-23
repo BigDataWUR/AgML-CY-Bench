97c6ae0e5067121a042124dd9123e4542f3795b4
krsnapaudel <dilli.paudel@wur.nl>
2024-02-23 09:51:07 +0100

    #70 prediction for non-existent locations

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -7,7 +7,16 @@ from models.naive_models import AverageYieldModel
 from config import PATH_DATA_DIR
 
 
-def test_predict_item():
+def get_model_predictions(model, sel_county, sel_year):
+    test_data = {
+        "COUNTY_ID": sel_county,
+        "FYEAR": sel_year,
+    }
+
+    return model.predict_item(test_data)
+
+
+def test_average_yield_model():
     model = AverageYieldModel(group_cols=["COUNTY_ID"], label_col="YIELD")
     data_sources = {
         "YIELD": {
@@ -21,14 +30,20 @@ def test_predict_item():
     yield_df = pd.read_csv(yield_csv, index_col=data_sources["YIELD"]["index_cols"])
     data_dfs = {"YIELD": yield_df}
     dataset = Dataset(data_dfs, data_sources, "COUNTY_ID", "FYEAR")
-    filtered_df = yield_df[yield_df.index.get_level_values(0) == "AL_AUTAUGA"]
-    expected_pred = filtered_df["YIELD"].mean()
     model.fit(dataset)
-    test_data = {
-        "COUNTY_ID": "AL_AUTAUGA",
-        "FYEAR": 2018,
-        "YIELD": yield_df.loc[("AL_AUTAUGA", 2018)]["YIELD"],
-    }
 
-    test_preds, _ = model.predict_item(test_data)
+    # test prediction for an existing item
+    sel_county = "AL_AUTAUGA"
+    sel_year = 2018
+    assert sel_county in yield_df.index.get_level_values(0)
+    filtered_df = yield_df[yield_df.index.get_level_values(0) == sel_county]
+    expected_pred = filtered_df["YIELD"].mean()
+    test_preds, _ = get_model_predictions(model, sel_county, sel_year)
+    assert np.round(test_preds[0], 2) == np.round(expected_pred, 2)
+
+    # test prediction for an existing item
+    sel_county = "CA_SAN_MATEO"
+    assert sel_county not in yield_df.index.get_level_values(0)
+    expected_pred = yield_df["YIELD"].mean()
+    test_preds, _ = get_model_predictions(model, sel_county, sel_year)
     assert np.round(test_preds[0], 2) == np.round(expected_pred, 2)
