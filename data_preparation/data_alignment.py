322605e0796bba0d21527515fb55f3675a96bbf5
krsnapaudel <dilli.paudel@wur.nl>
2024-03-04 11:16:44 +0100

    #83 align data with merge

--- data_preparation/data_alignment.py
+++ data_preparation/data_alignment.py
@@ -3,49 +3,49 @@ import pandas as pd
 
 from config import PATH_DATA_DIR
 
-def load_and_align_data(data_path, data_sources, label_key="YIELD"):
+
+def load_data_csv(data_path, data_sources):
     data_dfs = {}
-    # load the data
     for src in data_sources:
-        index_cols = data_sources[src]["index_cols"]
         data_csv = os.path.join(data_path, data_sources[src]["filename"])
-        df = pd.read_csv(data_csv, index_col=index_cols)
-        data_dfs[src] = df[data_sources[src]["sel_cols"]]
-
-    # align label data
-    label_df = data_dfs["YIELD"]
-    print("before merging", len(label_df.index))
-    for src in data_dfs:
-        if (src == label_key):
-            continue
-
-        df = data_dfs[src]
-        if (len(df.index.names) == 1):
-            # get intersection of locations
-            src_values = set(df.index.get_level_values(0))
-            label_values = set(label_df.index.get_level_values(0))
-            common_values = set.intersection(label_values, src_values)
-            # filter both by intersection
-            df = df.loc[common_values]
-            label_df = pd.concat([label_df.xs(key, level=0, drop_level=False) for key in common_values])
-        else:
-            label_values = label_df.index.values
-            # get values for index level 0 and 1
-            src_values = df.index.get_level_values()
-            # intersect the tuples
-
-        print("after merging", src, len(label_df.index))
-        print(label_df.sort_index().head())
-
-    # align other data
-    for src in data_dfs:
-        if (src == label_key):
-            continue
-
-        df = data_dfs[src]
-        df = df.merge(df, on=df.index.names)
+        df = pd.read_csv(data_csv, header=0)
+        data_dfs[src] = df
 
     return data_dfs
+
+def align_data(data_sources, label_df, feature_dfs, label_key="YIELD"):
+    # align label data
+    for src in feature_dfs:
+        df = feature_dfs[src]
+        index_cols = data_sources[src]["index_cols"]
+
+        # static data
+        if (len(index_cols) == 1):
+            index_cols = data_sources[src]["index_cols"]
+        else:
+            index_cols = data_sources[label_key]["index_cols"]
+
+        label_df = label_df.merge(df[index_cols].drop_duplicates(), on=index_cols, how="inner")
+        print("after merging", len(label_df.index))
+        print(label_df.sort_values(by=["COUNTY_ID", "FYEAR"]).head())
+
+    # align feature data
+    for src in feature_dfs:
+        df = feature_dfs[src]
+        print("before merging", src, len(df.index))
+        index_cols = data_sources[src]["index_cols"]
+
+        # static data
+        if (len(index_cols) == 1):
+            index_cols = data_sources[src]["index_cols"]
+        else:
+            index_cols = data_sources[label_key]["index_cols"]
+
+        df = df.merge(label_df[index_cols].drop_duplicates(), on=index_cols, how="inner")
+        print("after merging", len(df.index))
+        print(df.sort_values(by=index_cols).head())
+
+    return label_df, feature_dfs
 
 
 data_sources = {
@@ -67,4 +67,10 @@ data_sources = {
 }
 
 data_path = os.path.join(PATH_DATA_DIR, "data_US", "county_data")
-data_dfs = load_and_align_data(data_path, data_sources)
+data_dfs = load_data_csv(data_path, data_sources)
+label_df = data_dfs["YIELD"]
+feature_dfs = {
+    ft_key : data_dfs[ft_key] for ft_key in data_dfs if ft_key != "YIELD"
+}
+
+label_df, feature_dfs = align_data(data_sources, label_df, feature_dfs)
