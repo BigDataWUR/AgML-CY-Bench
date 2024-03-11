29a9e04b237c4839270b77a0615815c9673764c5
krsnapaudel <dilli.paudel@wur.nl>
2024-03-11 15:11:35 +0100

    #95 fix tests

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -25,12 +25,10 @@ def test_average_yield_model():
     yield_csv = os.path.join(data_path, "YIELD_COUNTY_US.csv")
     yield_df = pd.read_csv(yield_csv, index_col=[KEY_LOC, KEY_YEAR])
     dataset = Dataset(data_target=yield_df, data_features=[])
-    filtered_df = yield_df[yield_df.index.get_level_values(0) == "AL_AUTAUGA"]
-    expected_pred = filtered_df[KEY_TARGET].mean()
     model.fit(dataset)
 
     # test prediction for an existing item
-    sel_loc = "AL_AUTAUGA"
+    sel_loc = "US-01-001" # "AL_AUTAUGA"
     sel_year = 2018
     assert sel_loc in yield_df.index.get_level_values(0)
     filtered_df = yield_df[yield_df.index.get_level_values(0) == sel_loc]
@@ -39,7 +37,7 @@ def test_average_yield_model():
     assert np.round(test_preds[0], 2) == np.round(expected_pred, 2)
 
     # test prediction for a non-existent item
-    sel_loc = "CA_SAN_MATEO"
+    sel_loc = "US-06-081" # "CA_SAN_MATEO"
     assert sel_loc not in yield_df.index.get_level_values(0)
     expected_pred = yield_df[KEY_TARGET].mean()
     test_preds, _ = get_model_predictions(model, sel_loc, sel_year)
