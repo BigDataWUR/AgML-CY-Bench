b0673533a53a5db7c82442755bb64e2e19563e19
Pratishtha Poudel <poudelpratishtha@gmail.com>
2024-03-15 17:31:02 -0400

    Added the table of crop calendar data sources in README

--- data_preparation/shapefiles_US/get_US_shapefiles.R
+++ data_preparation/shapefiles_US/get_US_shapefiles.R
@@ -1,6 +1,19 @@
-library(sf)
-library(tidyverse)
-library(tigris)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(ncdf4, tidyverse, tigris, sf)
+
+# Uncomment the following line to download the shapefiles.
+#download.file("https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_500k.zip", destfile = "data_preparation/shapefiles_US/cb_2018_us_county_500k.zip", method = "wget", extra = "-r -p --random-wait")
+
+# Uncomment the following line to unzip the shapefiles
+# unzip("data_preparation/shapefiles_US/cb_2018_us_county_500k.zip", exdir = "data_preparation/shapefiles_US")
+
+# Load the shapefile
+counties <- st_read("data_preparation/shapefiles_US/cb_2018_us_county_500k.shp")
+
+# Plot the shapefile
+# ggplot(counties) + geom_sf()
+
+#################### Using tigris package ########################
 
 # We only need state names because counties doesn't have them.
 states_sf <- tigris::states(cb=T) |> 
@@ -21,5 +34,5 @@ county_sf <- tigris::counties(cb=T) |>
 #   geom_sf()
 
 # Write to a shapefile
-st_write(county_sf, "datasets/US_counties.shp", append=FALSE)
+#st_write(county_sf, "datasets/US_counties.shp", append=FALSE)
 
