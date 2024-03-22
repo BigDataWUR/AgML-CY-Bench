5c83b0edde88d53bfa57cad50559d249766d0555
Michiel Kallenberg <>
2024-03-22 15:47:46 +0100

    refactor a bit

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,30 +1,40 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+from config import KEY_TARGET
 from models.model import BaseModel
 from datasets.dataset import Dataset
 
 
+implemented_metrics = {}
+
+
+def metric(func):
+    """Decorator to mark functions as metrics"""
+    implemented_metrics[func.__name__] = func
+    return func
+
+
 def get_default_metrics():
-    return ('normalized_rmse', 'mape')
+    return ("normalized_rmse", "mape")
 
 
-def evaluate_model(model: BaseModel, dataset: Dataset, metrics=get_default_metrics(),):
+def evaluate_model(model: BaseModel, dataset: Dataset, metrics=get_default_metrics()):
     """
     Evaluate the performance of a model using specified metrics.
 
     Args:
       model: The trained model to be evaluated.
       dataset: Dataset.
-      metrics: List of metrics to calculate. Supported metrics: 'normalized_rmse', 'mape'.
+      metrics: List of metrics to calculate.
 
     Returns:
       A dictionary containing the calculated metrics.
     """
 
-    key_target = dataset.KEY_TARGET
     samples = [sample for sample in dataset]
 
-    y_true = [sample[key_target] for sample in samples]
+    y_true = [sample[KEY_TARGET] for sample in samples]
     y_true = np.array(y_true)
 
     y_pred, _ = model.predict_batch(samples)
@@ -33,31 +43,34 @@ def evaluate_model(model: BaseModel, dataset: Dataset, metrics=get_default_metri
     return results
 
 
-def evaluate_predictions(y_true: np.ndarray, y_pred: np.ndarray, metrics=get_default_metrics()):
+def evaluate_predictions(
+    y_true: np.ndarray, y_pred: np.ndarray, metrics=get_default_metrics()
+):
     """
     Evaluate predictions using specified metrics.
 
     Args:
       y_true (numpy.ndarray): True labels for evaluation.
       y_pred (numpy.ndarray): Predicted values.
-      metrics: List of metrics to calculate. Supported metrics: 'normalized_rmse', 'mape'
+      metrics: List of metrics to calculate.
 
     Returns:
       A dictionary containing the calculated metrics.
     """
-    results = {}
-    # Calculate metrics
-    if 'normalized_rmse' in metrics:
-        normalized_rmse_value = normalized_rmse(y_true, y_pred)
-        results['normalized_rmse'] = normalized_rmse_value
 
-    if 'mape' in metrics:
-        mape_value = mape(y_true, y_pred)
-        results['mape'] = mape_value
+    results = {}
+    for metric_name in metrics:
+        metric_function = implemented_metrics.get(metric_name)
+        if metric_function:
+            result = metric_function(y_true, y_pred)
+            results[metric_name] = result
+        else:
+            raise ValueError(f"Metric function '{metric_name}' not implemented.")
 
     return results
 
 
+@metric
 def normalized_rmse(y_true: np.ndarray, y_pred: np.ndarray):
     """
     Calculate the normalized Root Mean Squared Error (RMSE) between true and predicted values.
@@ -79,6 +92,7 @@ def normalized_rmse(y_true: np.ndarray, y_pred: np.ndarray):
     return 100 * np.sqrt(mse) / mean_y_true
 
 
+@metric
 def mape(y_true: np.ndarray, y_pred: np.ndarray):
     """
     Calculate Mean Absolute Percentage Error (MAPE).
@@ -101,6 +115,10 @@ def mape(y_true: np.ndarray, y_pred: np.ndarray):
     forecast_masked = y_pred[mask]  # y_pred is already a numpy array
 
     # Calculate MAPE using mean_absolute_error from sklearn
-    mape_value = 100 * mean_absolute_error(actual_masked, forecast_masked) / np.mean(actual_masked)
+    mape_value = (
+        100
+        * mean_absolute_error(actual_masked, forecast_masked)
+        / np.mean(actual_masked)
+    )
 
     return mape_value
