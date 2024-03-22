81a1e362c74dd921f7f66af4f2b13c0d8b2e172f
krsnapaudel <dilli.paudel@wur.nl>
2024-03-22 16:57:43 +0100

    save summary

--- datasets/dataset_overview.py
+++ datasets/dataset_overview.py
@@ -5,7 +5,7 @@ def get_crop_country_summary(crop, yield_df, adm_id_col, year_col):
     countries = yield_df[adm_id_col].str[:2].unique()
     row_idx = 0
     column_names = ["crop_name", "country_code", "min_year", "max_year", "num_years",
-                    "num_regions", "data_size"]
+                    "avg num_regions", "data_size"]
     for cn in countries:
       yield_cn_df = yield_df[yield_df[adm_id_col].str[:2] == cn]
       if (len(yield_cn_df.index) <= 1):
@@ -14,7 +14,8 @@ def get_crop_country_summary(crop, yield_df, adm_id_col, year_col):
       min_year = yield_cn_df[year_col].min()
       max_year = yield_cn_df[year_col].max()
       num_years = len(yield_cn_df[year_col].unique())
-      num_regions = yield_cn_df[yield_cn_df[year_col] == max_year][adm_id_col].count()
+      # average number of regions
+      num_regions = int(yield_cn_df.groupby([year_col]).size().mean())
       data_size = yield_cn_df[year_col].count()
       countries_summary["row" + str(row_idx)] = [crop, cn, min_year, max_year, num_years,
                                                 num_regions, data_size]
@@ -22,7 +23,7 @@ def get_crop_country_summary(crop, yield_df, adm_id_col, year_col):
 
     return countries_summary, column_names
 
-def print_data_summary(df):
+def print_data_summary(df, summary_file_prefix=""):
     crops = df["crop_name"].unique()
     for c in crops:
         countries_summary, column_names = get_crop_country_summary(c, df, "adm_id", "harvest_year")
@@ -31,6 +32,7 @@ def print_data_summary(df):
         num_countries = len(countries_summary_df.index)
         print("")
         print(countries_summary_df.head(num_countries).to_string())
+        countries_summary_df.to_csv(summary_file_prefix + "_" + c.replace(" ", "_") + "_summary.csv", index=False)
 
-eu_yields = pd.read_csv("data/Africa_crop_production.csv", header=0)
-print_data_summary(eu_yields)
+yields_df = pd.read_csv("data/YIELD_EU.csv", header=0)
+print_data_summary(yields_df, summary_file_prefix="EU")
