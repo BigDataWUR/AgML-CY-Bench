721f422c43e46197f544308b2dcf79695e9ef113
krsnapaudel <dilli.paudel@wur.nl>
2024-04-08 16:28:36 +0200

    #58 trend model clean version

---
+++ models/trend_model.py
@@ -0,0 +1,71 @@
+import pickle
+import numpy as np
+
+from models.model import BaseModel
+from datasets.dataset import Dataset
+
+from config import KEY_YEAR
+
+
+class TrendModel(BaseModel):
+    def __init__(self, x_cols, y_cols):
+        self._x_cols = x_cols
+        self._y_cols = y_cols
+
+    def fit(self, dataset: Dataset, **fit_params) -> tuple:
+        """Fit or train the model.
+        Args:
+          dataset: Dataset
+          **fit_params: Additional parameters.
+        Returns:
+          A tuple containing the fitted model and a dict with additional information.
+        """
+        # No training required.
+        return self, {}
+
+    def predict_batch(self, X: list):
+        """Run fitted model on batched data items.
+        Args:
+          X: a list of data items, each of which is a dict
+        Returns:
+          A tuple containing a np.ndarray and a dict with additional information.
+        """
+        predictions = np.zeros((len(X), 1))
+        for i, item in enumerate(X):
+            trend_x = [item[y] for y in self._x_cols]
+            trend_y = [item[c] for c in self._y_cols]
+            predictions[i] = self._get_trend(trend_x, trend_y, item[KEY_YEAR])
+
+        return predictions, {}
+
+    def _get_trend(self, trend_x, trend_y, pred_x):
+        """Implements a linear trend.
+        Args:
+          trend_x: a list of years.
+          trend_y: a list of values (e.g. yields)
+          pred_x: year for which to predict trend
+        Returns:
+          The trend based on linear trend of years and values
+        """
+        slope, coeff = np.polyfit(trend_x, trend_y, 1)
+        return (pred_x * slope + coeff)
+
+    def save(self, model_name):
+        """Save model, e.g. using pickle.
+        Args:
+          model_name: Filename that will be used to save the model.
+        """
+        with open(model_name, "wb") as f:
+            pickle.dump(self, f)
+
+    def load(cls, model_name):
+        """Deserialize a saved model.
+        Args:
+          model_name: Filename that was used to save the model.
+        Returns:
+          The deserialized model.
+        """
+        with open(model_name, "rb") as f:
+            saved_model = pickle.load(f)
+
+        return saved_model
--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -5,20 +5,13 @@ from sklearn.linear_model import Ridge
 
 from datasets.dataset import Dataset
 from models.naive_models import AverageYieldModel
+from models.trend_model import TrendModel
 from models.sklearn_model import SklearnModel
+from util.data import get_trend_features
 from evaluation.eval import evaluate_model
 
 from config import PATH_DATA_DIR
 from config import KEY_LOC, KEY_YEAR, KEY_TARGET
-
-
-def get_model_predictions(model, sel_loc, sel_year):
-    test_data = {
-        KEY_LOC: sel_loc,
-        KEY_YEAR: sel_year,
-    }
-
-    return model.predict_item(test_data)
 
 
 def test_average_yield_model():
@@ -35,14 +28,59 @@ def test_average_yield_model():
     assert sel_loc in yield_df.index.get_level_values(0)
     filtered_df = yield_df[yield_df.index.get_level_values(0) == sel_loc]
     expected_pred = filtered_df[KEY_TARGET].mean()
-    test_preds, _ = get_model_predictions(model, sel_loc, sel_year)
+    test_data = {
+        KEY_LOC: sel_loc,
+        KEY_YEAR: sel_year,
+    }
+
+    test_preds, _ = model.predict_item(test_data)
     assert np.round(test_preds[0], 2) == np.round(expected_pred, 2)
 
     # test prediction for a non-existent item
     sel_loc = "US-06-081"  # "CA_SAN_MATEO"
     assert sel_loc not in yield_df.index.get_level_values(0)
     expected_pred = yield_df[KEY_TARGET].mean()
-    test_preds, _ = get_model_predictions(model, sel_loc, sel_year)
+    test_data[KEY_LOC] = sel_loc
+    test_preds, _ = test_preds, _ = model.predict_item(test_data)
+    assert np.round(test_preds[0], 2) == np.round(expected_pred, 2)
+
+
+def test_trend_model():
+    trend_window = 5
+    x_cols = ["FYEAR-" + str(i) for i in range(trend_window, 0, -1)]
+    y_cols = ["YIELD-" + str(i) for i in range(trend_window, 0, -1)]
+    model = TrendModel(x_cols, y_cols)
+    data_path = os.path.join(PATH_DATA_DIR, "data_US", "county_data")
+    yield_csv = os.path.join(data_path, "YIELD_COUNTY_US.csv")
+    yield_df = pd.read_csv(yield_csv, header=0)
+    trend_fts, x_cols, y_cols = get_trend_features(yield_df, KEY_LOC, KEY_YEAR, KEY_TARGET, trend_window)
+
+    # align data
+    yield_df = yield_df.merge(trend_fts[[KEY_LOC, KEY_YEAR]], on=[KEY_LOC, KEY_YEAR])
+    yield_df = yield_df.set_index([KEY_LOC, KEY_YEAR])
+    trend_fts = trend_fts.set_index([KEY_LOC, KEY_YEAR])
+
+    # create dataset
+    dataset = Dataset(yield_df, feature_dfs=[trend_fts])
+    model.fit(dataset)
+
+    # dummy test data
+    dummy_x_vals = list(range(2016, 2021))
+    dummy_y_vals = list(range(2, 7))
+    sel_loc = "US-01-001"  # "AL_AUTAUGA"
+    sel_year = dummy_x_vals[-1] + 1
+    assert sel_loc in yield_df.index.get_level_values(0)
+
+    features = {}
+    for i, c in enumerate(x_cols):
+        features[c] = dummy_x_vals[i]
+
+    for i, c in enumerate(y_cols):
+        features[c] = dummy_y_vals[i]
+
+    # in dummy test data, yield increases by 1 every year
+    expected_pred = dummy_y_vals[-1] + 1
+    test_preds, _ = model.predict(sel_loc, sel_year, features)
     assert np.round(test_preds[0], 2) == np.round(expected_pred, 2)
 
 
