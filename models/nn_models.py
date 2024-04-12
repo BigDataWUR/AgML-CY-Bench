c5978394e064cdbd4df4e95fa28b8cd6a3527052
Aike Potze <aikepotze@hotmail.com>
2024-04-12 15:41:00 +0200

    First version of nn_models

--- models/nn_models.py
+++ models/nn_models.py
@@ -2,23 +2,23 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 
-from models.model import BaseModel
+from datasets.dataset_torch import TorchDataset
 from datasets.dataset import Dataset
+from models.model import BaseModel
+
+from config import KEY_LOC, KEY_YEAR, KEY_TARGET
+
 
 class BaseNNModel(BaseModel, nn.Module):
     def __init__(   self, 
-                    network: torch.nn.Module,
-                    device: str = "cpu",
                     *args, **kwargs):
-        
-        super().__init__(*args, **kwargs)
-        
-        self.network = network
-        self.device = device
+        super(BaseModel, self).__init__()
+        super(nn.Module, self).__init__()
+
 
     def fit(self, 
             train_dataset: Dataset,
-            val_dataset: Dataset = None,
+            val_fraction: float = 0.1,
             val_every_n_epochs: int = 1,
 
             num_epochs: int = 1,
@@ -33,57 +33,78 @@ class BaseNNModel(BaseModel, nn.Module):
             scheduler_fn: callable = None,
             scheduler_kwargs: dict = None,
 
+            device: str = "cpu",
+
              **fit_params):
         
         # Set default values for loss_fn, optim_fn, optim_kwargs, scheduler_fn, scheduler_kwargs
-        if loss_fn is None: loss_fn = nn.MSELoss
+        if loss_fn is None: loss_fn = torch.nn.functional.mse_loss
+        if loss_kwargs is None: loss_kwargs = {"reduction": "mean"}
         if optim_fn is None: optim_fn = torch.optim.Adam
         if optim_kwargs is None: optim_kwargs = {}
 
         assert num_epochs > 0
     
         # Send model to device
-        self.network.to(self.device)
+        self.to(device) #TODO this doesn't make sense
+        
         
         # Set optimizer and scheduler
-        optimizer = optim_fn(self.network.parameters(), **optim_kwargs)
-        if scheduler is not None: scheduler = scheduler_fn(optimizer, **scheduler_kwargs)
+        optimizer = optim_fn(self.parameters(), **optim_kwargs)
+        if scheduler_fn is not None: scheduler = scheduler_fn(optimizer, **scheduler_kwargs)
+
+        # Convert dataset to torch Dataset
+        train_dataset = TorchDataset(train_dataset)
+
+        # Get train and validation ids
+        n = len(train_dataset)
+        n_val = int(n * val_fraction)
+        n_train = n - n_val
+        train_ids = list(range(n_train))
+        val_ids = list(range(n_train, n))
+
 
         # Get dataloaders
-        train_batch_size = min(batch_size, len(train_dataset))
+        train_batch_size = min(batch_size, len(train_ids))
         train_loader = torch.utils.data.DataLoader(train_dataset, 
                                                    batch_size=train_batch_size, 
-                                                   shuffle=True,
+                                                   sampler=torch.utils.data.SubsetRandomSampler(train_ids),
                                                    collate_fn=train_dataset.collate_fn)
         
-        if val_loader is not None:
-            val_batch_size = min(batch_size, len(val_dataset))
-            val_loader = torch.utils.data.DataLoader(val_dataset, 
-                                                   batch_size=val_batch_size,
-                                                   collate_fn=val_dataset.collate_fn)
+        if val_fraction > 0:
+            val_batch_size = min(batch_size, len(val_ids))
+            val_loader = torch.utils.data.DataLoader(train_dataset, 
+                                                    batch_size=val_batch_size,
+                                                    sampler=torch.utils.data.SubsetRandomSampler(val_ids),
+                                                    collate_fn=train_dataset.collate_fn)
 
         # Load optimizer and scheduler
-        optimizer = optim_fn(self.network.parameters(), **optim_kwargs)
-        scheduler = scheduler_fn(optimizer, **scheduler_kwargs)
+        optimizer = optim_fn(self.parameters(), **optim_kwargs)
+        if scheduler_fn is not None: scheduler = scheduler_fn(optimizer, **scheduler_kwargs)
 
         # Training loop
         for epoch in range(num_epochs):
             losses = []
-            self.network.train()
+            self.train()
             pbar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}")
             for batch in pbar:
                 # Set gradients to zero
                 optimizer.zero_grad()
 
-                # TODO Should we normalize batch here?
-
                 # Send data to device
-                for key in batch: batch[key] = batch[key].to(self.device)
+                for key in batch: 
+                    if isinstance(batch[key], torch.Tensor):
+                        batch[key] = batch[key].to(device)
 
                 # Forward pass
-                predictions = self.network(batch)
+                predictions = self(batch).squeeze()
+                target = batch[KEY_TARGET]
 
-                target = batch[train_dataset.KEY_TARGET]
+                # Unsqueeze number of dimensions of target to match predictions
+                if len(predictions.shape) != len(target.shape):
+                    target = target.unsqueeze(-1)
+                target = target.float()
+
                 loss = loss_fn(predictions, target, **loss_kwargs)
 
                 # Backward pass
@@ -98,8 +119,8 @@ class BaseNNModel(BaseModel, nn.Module):
                 )
 
 
-            if val_dataset is not None and (epoch+1) % val_every_n_epochs == 0:
-                self.network.eval()
+            if val_loader is not None and (epoch+1) % val_every_n_epochs == 0:
+                self.eval()
 
                 # Validation loop
                 with torch.no_grad():
@@ -107,12 +128,20 @@ class BaseNNModel(BaseModel, nn.Module):
                     tqdm_val = tqdm(val_loader, desc=f"Validation Epoch {epoch+1}/{num_epochs}")
                     for batch in tqdm_val:
                         # Send data to device
-                        for key in batch: batch[key] = batch[key].to(self.device)
+                        for key in batch: 
+                            if isinstance(batch[key], torch.Tensor):
+                                batch[key] = batch[key].to(device)
 
                         # Forward pass
-                        predictions = self.network(batch)
+                        predictions = self(batch).squeeze()
 
-                        target = batch[val_dataset.KEY_TARGET]
+                        target = batch[KEY_TARGET]
+
+                        # Unsqueeze number of dimensions of target to match predictions
+                        if len(predictions.shape) != len(target.shape):
+                            target = target.unsqueeze(-1)
+                        target = target.float() #TODO move to dataloader
+
                         loss = loss_fn(predictions, target, **loss_kwargs)
 
                         # Save loss
@@ -122,19 +151,20 @@ class BaseNNModel(BaseModel, nn.Module):
                             f"Validation Epoch {epoch+1}/{num_epochs} | Loss: {loss.item():.4f}"
                         )
                     
-            scheduler.step()
+            if scheduler_fn is not None: scheduler.step()
         return self, {}
 
-    def predict_batch(self, X: list):
+    def predict_batch(self, X: list, device: str = "cpu"):
         # TODO this should be implemented per batch
+        # TODO fix self.device
 
         # Convert batch to tensor
-        X = torch.tensor(X, dtype=torch.float32, device=self.device)
-        self.network.to(self.device)
+        X = torch.tensor(X, dtype=torch.float32, device=device)
+        self.to(device)
 
-        self.network.eval()
+        self.eval()
         with torch.no_grad():
-            return self.network(X).detach(), {}
+            return self(X).detach(), {}
 
     def save(self, model_name):
         torch.save(self, model_name)
@@ -143,22 +173,57 @@ class BaseNNModel(BaseModel, nn.Module):
     def load(cls, model_name):
         return torch.load(model_name)
     
-class ExampleLSTM(nn.Module):
+class ExampleLSTM(BaseNNModel):
     def __init__(self, input_size, hidden_size, num_layers, output_size=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._bn1 = nn.BatchNorm1d(input_size)
         self._lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self._bn2 = nn.BatchNorm1d(output_size)
+        self._bn2 = nn.BatchNorm1d(hidden_size)
         self._fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        
-        # TODO load dict items correctly into tensor
+        # TODO: Keep this out of the model:
+        # 1. Get only the time series data from the batch
+        # 2. Ensure that there is always a batch dimension
+        # 3. Ensure that input data is float32
+        # 4. ?Concatenate along the channel dimension
+        # 5. Rearrange dimensions to (b, l, c)
+        # 6. Copy tensors in a better way, that does not give a warning
+
+
+        # Find the dimension of the input with the most dimensions, corresponding to time series data
+        max_n_dim = max([len(v.shape) for k, v in x.items() if isinstance(v, torch.Tensor)])
+
+        # Keep only the time series data from the batch
+        x = {k: v for k, v in x.items() if isinstance(v, torch.Tensor) and len(v.shape) == max_n_dim}
+
+        # Add batch dimension if not present
+        # If max_n_dim == 1, then the shape is (sequence_length)
+        # In this case we add a batch dimension and a channel dimension to get (b, c, l)
+        if max_n_dim == 1:
+            x = {k: v.unsqueeze(0).unsqueeze(1) for k, v in x.items()}
+        # If max_n_dim == 2, then the shape is (batch_size, n_features)
+        # In this case we add a channel dimension to get (b, c, l)
+        if max_n_dim == 2:
+            x = {k: v.unsqueeze(1) for k, v in x.items()}
+
+        # Concatenate along the channel dimension
+        x = torch.cat([v for k, v in x.items()], dim=1)
+
+        # Change dtype to float32 if needed
+        x = x.float() #TODO move to dataloader
 
         x = self._bn1(x)
+        x = x.permute(0, 2, 1)# Rearrange dimensions to (b, l, c)
         x, _ = self._lstm(x)
+        x = x.permute(0, 2, 1)# Rearrange dimensions to (b, c, l)
         x = self._bn2(x)
+        x = x.permute(0, 2, 1)# Rearrange dimensions to (b, l, c) #TODO check this
         x = self._fc(x)
+        # Dimensions of x: (batch_size, sequence_length, output_size)
+        # Take the last element of the sequence as the output
+        x = x[:, -1, :]
+        
         return x
     
 if __name__ == "__main__":
--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,14 +1,17 @@
 import os
+import torch as pt
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import Ridge
 
 from datasets.dataset import Dataset
+from datasets.dataset_torch import TorchDataset
 from models.naive_models import AverageYieldModel
 from models.sklearn_model import SklearnModel
-from models.nn_models import BaseNNModel, ExampleLSTM
+from models.nn_models import ExampleLSTM
 from config import PATH_DATA_DIR
 from config import KEY_LOC, KEY_YEAR, KEY_TARGET
+from data_preparation.data_alignment import load_data_csv, merge_data, set_indices
 
 
 def get_model_predictions(model, sel_loc, sel_year):
@@ -82,27 +85,72 @@ def test_sklearn_model():
     assert test_preds.shape[0] == len(test_dataset)
 
 def test_nn_model():
-    #TODO Finish this test
-    data_path = os.path.join(PATH_DATA_DIR, "data_US", "county_features")
-    # Training dataset
-    train_csv = os.path.join(data_path, "grain_maize_US_train.csv")
-    train_df = pd.read_csv(train_csv, index_col=["COUNTY_ID", "FYEAR"])
-    train_yields = train_df[["YIELD"]].copy()
-    feature_cols = [c for c in train_df.columns if c != "YIELD"]
-    train_features = train_df[feature_cols].copy()
-    train_dataset = Dataset(train_yields, [train_features])
+    data_sources = {
+            "YIELD" : {
+                "filename" : "YIELD_COUNTY_US.csv",
+                "index_cols" : [KEY_LOC, KEY_YEAR],
+                "sel_cols" : [KEY_TARGET]
+            },
+            "SOIL" : {
+                "filename" : "SOIL_COUNTY_US.csv",
+                "index_cols" : [KEY_LOC],
+                "sel_cols" : ["sm_whc"]
+            },
+            "REMOTE_SENSING" : {
+                "filename" : "REMOTE_SENSING_COUNTY_US.csv",
+                "index_cols" : [KEY_LOC, KEY_YEAR, "dekad"],
+                "sel_cols" : ["fapar"]
+            }
+        }
+    
+    device = pt.device("cuda" if pt.cuda.is_available() else "cpu")
 
-    # Test dataset
-    test_csv = os.path.join(data_path, "grain_maize_US_train.csv")
-    test_df = pd.read_csv(test_csv, index_col=["COUNTY_ID", "FYEAR"])
-    test_yields = test_df[["YIELD"]].copy()
-    test_features = test_df[feature_cols].copy()
-    test_dataset = Dataset(test_yields, [test_features])
+    data_path = os.path.join(PATH_DATA_DIR, "data_US", "county_data")
+    data_dfs = load_data_csv(data_path, data_sources)
+    label_df = data_dfs["YIELD"]
 
-    # Model
-    network = ExampleLSTM(input_size=len(feature_cols), hidden_size=64, num_layers=2)
-    model = BaseNNModel(network, device="cpu")
-    model.fit(train_dataset)
+    feature_dfs = {
+        ft_key : data_dfs[ft_key] for ft_key in data_dfs if ft_key != "YIELD"
+    }
+
+    label_df, feature_dfs = merge_data(data_sources, label_df, feature_dfs)
+    label_df, feature_dfs = set_indices(data_sources, label_df, feature_dfs)
+
+    # Sort the indices
+    label_df.sort_index(inplace=True)
+    for src in feature_dfs:
+        feature_dfs[src].sort_index(inplace=True)
+
+    # Convert dict of dataframes to list of dataframes, throw away the keys
+    feature_dfs = list(feature_dfs.values())
+
+    train_dataset = TorchDataset(Dataset(label_df, feature_dfs))
+    test_dataset = TorchDataset(Dataset(label_df, feature_dfs))
+
+    # Initialize model
+    time_series_features = [key for key in dataset[0].keys() if type(dataset[0][key]) == pt.Tensor]
+    time_series_features = [key for key in time_series_features if dataset[0][key].dim() > 0]
+    model = ExampleLSTM(input_size=len(time_series_features), hidden_size=64, num_layers=2, output_size=1)
+
+    # Train model
+    model.fit(dataset, batch_size=3200, num_epochs=1, device=device)
+
     test_preds, _ = model.predict(test_dataset)
     assert test_preds.shape[0] == len(test_dataset)
+
+    evaluation_result = evaluate_model(model, test_dataset)
+
+
+    expected_values = {
+        "normalized_rmse": 14.49,
+        "mape": 0.14,
+    }
+    for metric, expected_value in expected_values.items():
+        assert (
+            metric in evaluation_result
+        ), f"Metric '{metric}' not found in evaluation result"
+        assert (
+            round(evaluation_result[metric], 2) == expected_value
+        ), f"Value of metric '{metric}' does not match expected value"
+
 
