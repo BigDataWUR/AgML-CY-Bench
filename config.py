0728f06c972fcf926bbdcad2ecc49bde7ce1eca3
krsnapaudel <dilli.paudel@wur.nl>
2024-04-24 15:32:05 +0200

    #140 add logging to file

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from datetime import datetime
 
 
 # Project root dir
@@ -14,3 +16,12 @@ KEY_LOC = "loc_id"
 KEY_YEAR = "year"
 # Key used for yield targets
 KEY_TARGET = "yield"
+
+# Logging path
+PATH_LOGS_DIR = os.path.join(CONFIG_DIR, "logs")
+os.makedirs(PATH_LOGS_DIR, exist_ok=True)
+
+# Logging level
+LOGGER_NAME = "agml_cybench"
+LOG_FILE = datetime.now().strftime(LOGGER_NAME + "_%H_%M_%d_%m_%Y.log")
+LOG_LEVEL = logging.DEBUG
