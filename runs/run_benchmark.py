442c89c1fd5c081f98091efa3f5c874f681b66e6
ronvree <ronvbree@gmail.com>
2024-05-03 15:23:41 +0200

    outline

--- runs/run_benchmark.py
+++ runs/run_benchmark.py
@@ -1,19 +1,69 @@
+from collections import defaultdict
 
 from models.model import BaseModel
 
 from datasets.dataset import Dataset
 
-from evaluation.eval import evaluate_model
+from evaluation.eval import evaluate_model, evaluate_predictions
+
+from models.naive_models import AverageYieldModel
+from models.sklearn_model import SklearnModel
+from models.nn_models import ExampleLSTM
+
+
+def run_benchmark(model_name, model_constructor, model_kwargs):
+    benchmark_models = {
+        "AverageYieldModel": AverageYieldModel,
+        "LSTM": ExampleLSTM,
+        model_name: model_constructor
+    }
+    models_kwargs = defaultdict(dict)
+    models_kwargs[model_name] = model_kwargs
+
+    dataset = Dataset.load("test_maize_us")
+
+    all_years = dataset.years
+    for test_year in all_years:
+        train_years = [y for y in all_years if y != test_year]
+        test_years = [test_year]
+        train_dataset, test_dataset = dataset.split_on_years((train_years, test_years))
+
+        compiled_results = {}
+        for model_name, model_constructor in benchmark_models.items():
+            model = model_constructor(**models_kwargs[model_name])
+            model.fit(train_dataset)
+            predictions, _ = model.predict(test_dataset)
+            # save predictions
+            labels = test_dataset.targets()
+            results = evaluate_predictions(labels, predictions)
+            compiled_results[model_name] = results
+
+    return compiled_results
 
 
 
 
+if __name__ == '__main__':
 
-def run_benchmark(model: BaseModel,
-                  dataset_name: str,
-                  model_fit_kwargs: dict,
-                  verbose: bool = True,
-                  ) -> dict:
+    run_benchmark(model_name="AverageYieldModel",
+                  model_constructor=AverageYieldModel,
+                  model_kwargs={},
+                  )
+
+
+#
+# def evaluate_saved_predictions():
+#     pass
+
+
+# def run_benchmark(model: BaseModel,
+#                   dataset_name: str,
+#                   model_fit_kwargs: dict,
+#                   verbose: bool = True,
+#                   ) -> dict:
+
+
+
 
 
     # For all algorithms in the benchmark
@@ -32,33 +82,73 @@ def run_benchmark(model: BaseModel,
 
 
 
+#
+#     # dataset_train, dataset_test = Dataset.load(dataset_name)
+#     dataset = Dataset.load(dataset_name)
+#     dataset_train, dataset_test = Dataset.split(...)
+#
+#     model.fit(dataset_train, **model_fit_kwargs)
+#
+#     eval_result_train = evaluate_model(model,
+#                                        dataset_train,
+#                                        )
+#     eval_result_test = evaluate_model(model,
+#                                       dataset_test,
+#                                       )
+#
+#     return {
+#         'model': model,
+#         'eval_train': eval_result_train,
+#         'eval_test': eval_result_test,
+#     }
+#
+#
+# def _create_table(eval_results: dict, title: str) -> list:
+#
+#     pass
 
-    # dataset_train, dataset_test = Dataset.load(dataset_name)
-    dataset = Dataset.load(dataset_name)
-    dataset_train, dataset_test = Dataset.split(...)
-
-    if isinstance(model, ...):  # TODO -- check if model is a torch model
-        model.train()
-
-    model.fit(dataset_train, **model_fit_kwargs)
-
-    if isinstance(model, ...):  # TODO -- check if model is a torch model
-        model.eval()
-
-    eval_result_train = evaluate_model(model,
-                                       dataset_train,
-                                       )
-    eval_result_test = evaluate_model(model,
-                                      dataset_test,
-                                      )
-
-    return {
-        'model': model,
-        'eval_train': eval_result_train,
-        'eval_test': eval_result_test,
-    }
 
 
-def _create_table(eval_results: dict, title: str) -> list:
-    
-    pass
+# from datasets.dataset import Dataset
+# from models.naive_models import AverageYieldModel
+# from models.sklearn_model import SklearnModel
+# from models.nn_models import ExampleLSTM
+# from evaluation.eval import evaluate_predictions
+# from collections import defaultdict
+#
+# from config import KEY_LOC, KEY_YEAR, KEY_TARGET
+# from config import SOIL_COLS, REMOTE_SENSING_COLS, METEO_COLS
+#
+#
+# def run_benchmark(model_name, model_constructor, model_kwargs):
+#     benchmark_models = {
+#         "AverageYieldModel" : AverageYieldModel,
+#         "LSTM" : ExampleLSTM,
+#         model_name : model_constructor
+#     }
+#     models_kwargs = defaultdict(dict)
+#     models_kwargs[model_name] = model_kwargs
+#
+#     dataset = Dataset.load("maize_us")
+#     all_years = dataset.years
+#     for test_year in all_years:
+#         train_years = [y for y in all_years if y != test_year]
+#         test_years = [test_year]
+#         train_dataset, test_dataset = dataset.split_on_years((train_years, test_years))
+#
+#         compiled_results = {}
+#         for model_name, model_constructor in benchmark_models.items():
+#             model = model_constructor(**models_kwargs[model_name])
+#             model.fit(train_dataset)
+#             predictions, _ = model.predict(test_dataset)
+#             # save predictions
+#             labels = test_dataset.labels
+#             results = evaluate_predictions(labels, predictions)
+#             compiled_results[model_name] = results
+#
+#     return compiled_results
+#
+# def evaluate_saved_predictions():
+#     pass
+
+
