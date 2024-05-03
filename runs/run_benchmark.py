f3ba6a67e725e36893e78b731b9e61d9413f79fd
ronvree <ronvbree@gmail.com>
2024-05-03 14:56:16 +0200

    outline

--- runs/run_benchmark.py
+++ runs/run_benchmark.py
@@ -6,13 +6,36 @@ from datasets.dataset import Dataset
 from evaluation.eval import evaluate_model
 
 
+
+
+
 def run_benchmark(model: BaseModel,
                   dataset_name: str,
                   model_fit_kwargs: dict,
                   verbose: bool = True,
                   ) -> dict:
 
-    dataset_train, dataset_test = Dataset.load(dataset_name)
+
+    # For all algorithms in the benchmark
+    #    For all years that span the benchmark dataset
+    #        do train test split
+    #        initialize a model
+    #        fit on the training dataset
+    #        predict on test dataset
+    #        save predictions to csv
+
+    # For all algorithms in the benchmark
+    #    For all years that span the benchmark dataset
+    #         load csv of predictions
+    #         load csv of reference predictions in same fold
+    #         compute metrics
+
+
+
+
+    # dataset_train, dataset_test = Dataset.load(dataset_name)
+    dataset = Dataset.load(dataset_name)
+    dataset_train, dataset_test = Dataset.split(...)
 
     if isinstance(model, ...):  # TODO -- check if model is a torch model
         model.train()
