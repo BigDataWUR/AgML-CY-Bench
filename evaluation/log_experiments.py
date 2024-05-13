1f7ee71fb59aa82a9d9e42e94e1eda4b49e364af
hbja <hbja96@gmail.com>
2024-05-13 11:13:20 +0200

    Added some comet functions: ability to start an anonymous experiment to claim later, wrap model for automatic metric logging, get comet api key from file, etc.

--- evaluation/log_experiments.py
+++ evaluation/log_experiments.py
@@ -1,36 +1,100 @@
 import os
 
+import comet_ml.integration.pytorch
 from comet_ml import Experiment
 import torch
 import sklearn
+from models.model import BaseModel
 from models.sklearn_model import SklearnModel
 from models.trend_model import TrendModel
 from models.naive_models import AverageYieldModel
+from models.nn_models import BaseNNModel
 
 # get paths
 evaluation_path = os.path.dirname(os.path.realpath(__file__))
 root_path = evaluation_path[:-10]
 
-# TODO set path
-with open(os.path.join(evaluation_path, 'api_keys', 'comet_ml'), 'r') as f:
-    api_key = f.readline()
-
 
 # Configure Comet experiment instance
-
-comet_api_key = api_key
-project_name = "Crop-yield-forecasting"
-workspace = "AgML"
+_project_name = "AgML-Crop-yield-forecasting"
 
 
-def log_to_comet(metrics: dict,
-                 params: dict,
-                 name: str = None,
-                 model: str=None,
-                 asset_path: str=None):
+# get api key
+def get_comet_api_key(file=None) -> str:
+    """
+    Function that returns api_key.
+
+    Store the api key under evaluation/api_keys/...
+
+    :param: file: file name that contains one line of the comet api key.
+    """
+
+    api_key = None
+
+    check_api_key_dir = os.path.isdir(os.path.join(evaluation_path, 'api_keys'))
+    file_name = 'comet_ml' if None else file
+    check_api_key_file = os.path.isdir(os.path.join(evaluation_path, 'api_keys', file_name))
+
+    if check_api_key_dir and check_api_key_file:
+        with open(os.path.join(evaluation_path, 'api_keys', 'comet_ml'), 'r') as f:
+            api_key = f.readline()
+    return api_key
+
+
+def existing_comet(comet_experiment: Experiment,
+                   comet_api_key: str = None,
+                   project_name: str = None,
+                   workspace: str = None) -> Experiment:
+    """
+    If api_key is not defined, comet_ml will start in anonymous mode and the logs can be accessed
+        through the URL provided in the console
+
+    :return: comet Experiment object
+    """
+
+    if comet_api_key is None:
+        comet_api_key = get_comet_api_key()
+
+    if comet_experiment is None:
+        if comet_api_key is not None:
+            experiment = Experiment(
+                api_key=comet_api_key,
+                project_name=_project_name if project_name is None else project_name,
+                workspace=workspace,
+                log_env_gpu=True,
+                auto_log_co2=True,
+                auto_metric_logging=True,
+                auto_param_logging=True,
+                auto_histogram_gradient_logging=True
+            )
+        else:
+            comet_ml.init(anonymous=True)
+            experiment = Experiment(
+                project_name=_project_name if project_name is None else project_name,
+                workspace=workspace,
+                log_env_gpu=True,
+                auto_log_co2=True,
+                auto_metric_logging=True,
+                auto_param_logging=True,
+                auto_histogram_gradient_logging=True
+            )
+    else:
+        experiment = comet_experiment
+
+    return experiment
+
+
+def log_to_comet_post_hoc(metrics: dict,
+                          params: dict,
+                          comet_experiment: Experiment = None,
+                          comet_api_key: str = None,
+                          name: str = None,
+                          model: str = None,
+                          asset_path: str = None) -> None:
     """
     Log metrics, params, asset and model to Comet_ml
 
+    :param comet_experiment: comet experiment object
     :param metrics: dict of metrics
     :param params: dict of params/hyperparams
     :param model: name of the saved model
@@ -38,11 +102,8 @@ def log_to_comet(metrics: dict,
     :param asset_path: path to asset. To log a custom asset (e.g., config file etc.) to Comet.
     :return:
     """
-    experiment = Experiment(
-        api_key=comet_api_key,
-        project_name=project_name,
-        workspace=workspace
-    )
+
+    experiment = existing_comet(comet_experiment=comet_experiment, comet_api_key=comet_api_key)
     experiment.log_parameters(params)
     experiment.log_metrics(metrics)
     experiment.log_code(folder=root_path)
@@ -65,3 +126,16 @@ def log_to_comet(metrics: dict,
         experiment.set_name(f"CYM-model")
 
     experiment.end()
+
+
+def comet_wrapper(model: BaseModel,
+                  comet_experiment: Experiment = None,
+                  comet_api_key: str = None) -> None:
+    """Wrap model before training with a Comet experiment instance"""
+
+    experiment = existing_comet(comet_experiment=comet_experiment, comet_api_key=comet_api_key)
+
+    experiment.log_code(folder=root_path)
+
+    if isinstance(model, BaseNNModel):
+        comet_ml.integration.pytorch.watch(model, 1)
