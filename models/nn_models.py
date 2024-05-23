77428031525327d0c83153332a81e675c6a665cb
Aike Potze <aikepotze@hotmail.com>
2024-05-23 11:37:31 +0200

    added kfold cv tuning

--- models/nn_models.py
+++ models/nn_models.py
@@ -1,11 +1,16 @@
+import copy
+import itertools
+import random
 import torch
 import torch.nn as nn
 from tqdm import tqdm
 import numpy as np
+from sklearn.model_selection import ParameterGrid
 
 from datasets.dataset_torch import TorchDataset
 from datasets.dataset import Dataset
 from models.model import BaseModel
+from util.data import flatten_nested_dict, unflatten_nested_dict
 
 from config import KEY_LOC, KEY_YEAR, KEY_TARGET
 
@@ -16,9 +21,68 @@ class BaseNNModel(BaseModel, nn.Module):
         super(BaseModel, self).__init__()
         super(nn.Module, self).__init__()
 
+        self._init_args = (args, kwargs)
 
-    def fit(self, 
+
+    def fit(self, dataset: Dataset,
+            optimize_hyperparameters: bool = False,
+            param_space: dict = None,
+            do_kfold: bool = False,
+            kfolds: int = 5,
+            *args, **kwargs):
+        
+        if optimize_hyperparameters:
+            assert param_space is not None
+
+            best_loss = float("inf")
+            best_setting = None
+
+            settings = self._generate_settings(param_space, kwargs)
+            for setting in settings:
+                val_loss = None
+                if do_kfold:
+                    # Split data into k folds
+                    all_years = dataset.years
+
+                    list_all_years = list(all_years)
+                    random.shuffle(list_all_years) # Is this seeded?
+                    cv_folds = [list_all_years[i::kfolds] for i in range(kfolds)]
+
+                    val_loss_fold = []
+                    for val_fold in cv_folds:
+                        # Create train and val datasets for inner fold
+                        val_years = val_fold
+                        train_years = [y for y in all_years if y not in val_years]
+                        train_dataset, val_dataset = dataset.split_on_years((train_years, val_years))
+                        new_model = self.__class__(*self._init_args[0], **self._init_args[1])
+                        _, output = new_model.train(train_dataset=train_dataset, val_dataset=val_dataset, *args, **setting)
+                        val_loss_fold.append(output["val_loss"])
+                    val_loss = np.mean(val_loss_fold)
+
+                else:
+                    new_model = self.__class__(*self._init_args[0], **self._init_args[1])
+                    _, output = new_model.fit(*args, **setting)
+                    if "val_loss" not in output:
+                        raise ValueError("No validation loss recorder, set val_fraction > 0 when tuning without kfold cross validation.")
+                    val_loss = output["val_loss"]
+                
+                assert val_loss is not None
+
+                # Store best model setting
+                if val_loss < best_loss:
+                    best_loss = val_loss
+                    best_setting = setting
+            
+            return self.fit(*args, **best_setting)
+        else:
+           
+            model, output = self.train(*args, **kwargs)
+            return model, output
+        
+
+    def train(self, 
             train_dataset: Dataset,
+            val_dataset: Dataset = None,
             val_fraction: float = 0.1,
             val_every_n_epochs: int = 1,
 
@@ -53,12 +117,12 @@ class BaseNNModel(BaseModel, nn.Module):
             scheduler_fn: callable, the scheduler function, default is None
             scheduler_kwargs: dict, additional parameters for the scheduler function, default is {}
             device: str, the device to use, default is "cpu"
+
             **fit_params: Additional parameters.
 
         Returns:
           A tuple containing the fitted model and a dict with additional information.
         """
-        
 
         # Set default values 
         if loss_fn is None: loss_fn = torch.nn.functional.mse_loss
@@ -78,28 +142,41 @@ class BaseNNModel(BaseModel, nn.Module):
         # Get torchdataset, random val indices and loader
         train_dataset = TorchDataset(train_dataset)
 
-        n = len(train_dataset)
-        n_val = int(n * val_fraction)
-        val_ids = np.random.choice(n, n_val, replace=False).tolist()
-        train_ids = list(set(range(n)) - set(val_ids))
+        if val_dataset is not None:
+            val_dataset = TorchDataset(val_dataset)
+            train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, collate_fn=train_dataset.collate_fn, shuffle=True, drop_last=True)
+            val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, collate_fn=val_dataset.collate_fn)
+        else:
+            n = len(train_dataset)
+            n_val = int(n * val_fraction)
+            val_ids = np.random.choice(n, n_val, replace=False).tolist()
+            train_ids = list(set(range(n)) - set(val_ids))
+            assert len(set(val_ids).intersection(set(train_ids))) == 0
 
-        train_batch_size = min(batch_size, len(train_ids))
-        train_loader = torch.utils.data.DataLoader(train_dataset, 
-                                                   batch_size=train_batch_size, 
-                                                   sampler=torch.utils.data.SubsetRandomSampler(train_ids),
-                                                   collate_fn=train_dataset.collate_fn)
-        
-        if val_fraction > 0:
-            val_batch_size = min(batch_size, len(val_ids))
-            val_loader = torch.utils.data.DataLoader(train_dataset, 
-                                                    batch_size=val_batch_size,
+            train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                                    batch_size=batch_size, 
+                                                    sampler=torch.utils.data.SubsetRandomSampler(train_ids),
+                                                    collate_fn=train_dataset.collate_fn)
+            if val_fraction > 0:
+                val_loader = torch.utils.data.DataLoader(train_dataset, 
+                                                    batch_size=batch_size,
                                                     sampler=torch.utils.data.SubsetRandomSampler(val_ids),
                                                     collate_fn=train_dataset.collate_fn)
-        else: val_loader = None
+            else: val_loader = None
+        
 
         # Load optimizer and scheduler
         optimizer = optim_fn(self.parameters(), **optim_kwargs)
         if scheduler_fn is not None: scheduler = scheduler_fn(optimizer, **scheduler_kwargs)
+
+        # Store training set feature means and sds for normalization
+        self.feature_means = {}
+        self.feature_sds = {}
+        all_train_samples = TorchDataset.collate_fn([train_dataset[i] for i in range(len(train_dataset))])
+        for key, features in all_train_samples.items():
+            if key not in [KEY_TARGET, KEY_LOC, KEY_YEAR]:
+                self.feature_means[key] = features.mean()
+                self.feature_sds[key] = features.std()
 
         # Training loop
         for epoch in range(num_epochs):
@@ -116,6 +193,13 @@ class BaseNNModel(BaseModel, nn.Module):
 
                 # Forward pass
                 features = {k: v for k, v in batch.items() if k != KEY_TARGET}
+
+                # Normalize features
+                for key in features:
+                    if key not in [KEY_LOC, KEY_YEAR]:
+                        features[key] = (features[key] - self.feature_means[key]) / self.feature_sds[key]
+
+
                 predictions = self(features)
                 if predictions.dim() > 1: predictions = predictions.squeeze(-1)
                 target = batch[KEY_TARGET]
@@ -131,7 +215,7 @@ class BaseNNModel(BaseModel, nn.Module):
                 )
 
 
-            if val_loader is not None and (epoch+1) % val_every_n_epochs == 0:
+            if val_loader is not None and (epoch % val_every_n_epochs == 0 or epoch == num_epochs - 1):
                 self.eval()
 
                 # Validation loop
@@ -144,6 +228,10 @@ class BaseNNModel(BaseModel, nn.Module):
                                 batch[key] = batch[key].to(device)
 
                         features = {k: v for k, v in batch.items() if k != KEY_TARGET}
+                        # Normalize features
+                        for key in features:
+                            if key not in [KEY_LOC, KEY_YEAR]:
+                                features[key] = (features[key] - self.feature_means[key]) / self.feature_sds[key]
                         predictions = self(features)
                         if predictions.dim() > 1: predictions = predictions.squeeze(-1)
                         target = batch[KEY_TARGET]
@@ -156,7 +244,7 @@ class BaseNNModel(BaseModel, nn.Module):
                         )
                     
             if scheduler_fn is not None: scheduler.step()
-        return self, {}
+        return self, {"train_loss": np.mean(losses.detach().cpu().numpy()), "val_loss": np.mean(val_losses.detach().cpu().numpy()) if val_loader is not None else None}
 
     def predict_batch(self, X: list, device: str = None, batch_size: int = None):
         """Run fitted model on batched data items.
@@ -183,6 +271,9 @@ class BaseNNModel(BaseModel, nn.Module):
                 batch = TorchDataset.collate_fn([TorchDataset._cast_to_tensor(sample) for sample in batch])
                 batch = {key: batch[key].to(device) for key in batch.keys() if isinstance(batch[key], torch.Tensor)}
                 features = {k: v for k, v in batch.items() if k != KEY_TARGET}
+                for key in features:
+                    if key not in [KEY_LOC, KEY_YEAR]:
+                        features[key] = (features[key] - self.feature_means[key]) / self.feature_sds[key]
                 y_pred = self(features)
                 if y_pred.dim() > 1: y_pred = y_pred.squeeze(-1)
                 y_pred = y_pred.cpu().numpy()
@@ -210,6 +301,30 @@ class BaseNNModel(BaseModel, nn.Module):
         """
         return torch.load(model_name)
     
+    
+    def _generate_settings(self,
+                           param_space: dict,
+                           standard_settings: dict):
+        
+        # Flatten both dicts
+        param_space = flatten_nested_dict(param_space)
+        standard_settings = flatten_nested_dict(standard_settings)
+
+        # Generate all combinations of parameter values
+        combs = list(ParameterGrid(param_space))
+
+        # For each comb, deepcopy standard_settings and replace values
+        settings = []
+        for comb in combs:
+            setting = copy.deepcopy(standard_settings)
+            setting.update(comb)
+            settings.append(setting)
+
+        # Unflatten settings
+        settings = [unflatten_nested_dict(setting) for setting in settings]
+
+        return settings
+    
 
 class ExampleLSTM(BaseNNModel):
     def __init__(self, n_ts_features, n_static_features, hidden_size, num_layers, output_size=1, *args, **kwargs):
@@ -221,6 +336,9 @@ class ExampleLSTM(BaseNNModel):
         max_n_dim = max([len(v.shape) for k, v in x.items() if isinstance(v, torch.Tensor)])
         x_ts = torch.cat([v.unsqueeze(2) for k, v in x.items() if isinstance(v, torch.Tensor) and len(v.shape) == max_n_dim], dim=2)
         x_static = torch.cat([v.unsqueeze(1) for k, v in x.items() if isinstance(v, torch.Tensor) and len(v.shape) < max_n_dim], dim=1)
+
+        print(x_ts.mean(), x_ts.std())
+        print(x_static.mean(), x_static.std())
 
         x_ts, _ = self._lstm(x_ts)
         x = torch.cat([x_ts[:, -1, :], x_static], dim=1)
--- util/data.py
+++ util/data.py
@@ -11,3 +11,31 @@ def data_to_pandas(data_items):
         data.append([item[c] for c in data_cols])
 
     return pd.DataFrame(data, columns=data_cols)
+
+
+def flatten_nested_dict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_nested_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+def unflatten_nested_dict(d, sep='.'):
+    out = {}
+    for k, v in d.items():
+        keys = k.split(sep)
+        if len(keys) == 1:
+            out[k] = v
+        else:
+            new_key = keys[0]
+            new_subkey = sep.join(keys[1:])
+            if new_key not in out: out[new_key] = {}
+            out[new_key][new_subkey] = v
+    for k, v in out.items():
+        if isinstance(v, dict):
+            out[k] = unflatten_nested_dict(v, sep=sep)
+    return out
