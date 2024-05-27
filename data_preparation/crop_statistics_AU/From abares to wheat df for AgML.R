6d872cb89fa6afdee2c56d4fb12c429298b6e48f
Jonathan Richetti <ric375@petrichor-login.cm.cluster>
2024-05-27 18:35:29 +1000

    updated readme and R script

--- data_preparation/crop_statistics_AU/From abares to wheat df for AgML.R
+++ data_preparation/crop_statistics_AU/From abares to wheat df for AgML.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(sf)
 
-loc = '/datasets/work/af-sandpit/work/Jonathan/AgML/AgML-crop-yield-forecasting/data_preparation/'
+loc = '/datasets/work/af-sandpit/work/Jonathan/AgML/'
+#AgML-crop-yield-forecasting/data_preparation/'
 
 # loading data from gov website ; download on 05/03/2024
-df = read.csv("fdp-beta-regional-historical.csv") #file origin https://www.agriculture.gov.au/sites/default/files/documents/fdp-beta-regional-historical.csv
+df = read.csv(paste0(loc,"fdp-beta-regional-historical.csv")) #file origin https://www.agriculture.gov.au/sites/default/files/documents/fdp-beta-regional-historical.csv
 head(df)
 
 # filtering wheat production and area swon
@@ -17,11 +18,14 @@ names(temp_df) = c('year','NAME','production','planted_area')
 head(temp_df)
 
 # grabbing the AAGIS ID from the shapefile
-shape = read_sf('abares_regions/ABARES_regions_boundaries.shp')
-shape_df =  shape %>% select(AAGIS,NAME) %>% st_drop_geometry()
+shape = read_sf(paste0(loc,'ABARES_regions_boundaries.shp'))
+shape['adm_id'] = paste0('AU-',shape$AAGIS) # creates adm_id as AgML request
+write_sf(shape, paste0(loc,'ABARES_regions_boundaries.shp')) # saves with adm_id
+
+shape_df =  shape %>% select(adm_id,NAME) %>% st_drop_geometry()
 wheat_df = merge(temp_df, shape_df, by='NAME', all.x = T)
 
-names(wheat_df) = c('NAME','harvest_year','production','planted_area','adm_id')
+names(wheat_df) = c('NAME','harvest_year','production','planted_area','adm_id') # naming convention for AgML
 
 #dropping name column
 wheat_df$NAME = NULL
@@ -38,22 +42,24 @@ wheat_df$harvest_area= NA
 
 head(wheat_df)
 
-write.csv(wheat_df,'wheat_Australia.csv')
+# save .csv as AgML
+write.csv(wheat_df,paste0(loc,'wheat_Australia.csv'))
 
-ggplot(wheat_df)+
-  geom_line(aes(x=harvest_year,y=yield))+
-  facet_wrap(~adm_id)+
-  theme_bw()
+# no need ----
+#ggplot(wheat_df)+
+#    geom_line(aes(x=harvest_year,y=yield))+
+#    facet_wrap(~adm_id)+
+#    theme_bw()
 
 # Creating joined shape with statistics
-wheat_df = read.csv(paste0(loc,'crop_statistics_AU/','wheat_Australia.csv'))
-head(wheat_df)
+#wheat_df = read.csv(paste0(loc,'crop_statistics_AU/','wheat_Australia.csv'))
+#head(wheat_df)
 
-shape = read_sf(paste0(loc,'shapefiles_AU/','ABARES_regions_boundaries.shp'))
-names(shape) = c('ste','adm_id','oid_','name','geometry')
-head(shape)
+#shape = read_sf(paste0(loc,'shapefiles_AU/','ABARES_regions_boundaries.shp'))
+#names(shape) = c('ste','adm_id','oid_','name','geometry')
+#head(shape)
 
-new_shape = merge(wheat_df, shape, by='adm_id', all.x = T)
-head(new_shape)
+#new_shape = merge(wheat_df, shape, by='adm_id', all.x = T)
+#head(new_shape)
 
-write_sf(new_shape, paste0(loc,'shapefiles_AU/','ABARES_regions_with_stats.shp'))
+#write_sf(new_shape, paste0(loc,'shapefiles_AU/','ABARES_regions_with_stats.shp'))
