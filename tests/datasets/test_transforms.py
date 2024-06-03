022a895a82ba60a4991f4f6c4ea948176d8621db
Aike Potze <aikepotze@hotmail.com>
2024-06-03 16:37:56 +0200

    changed test dataset and removed temporary fix

--- tests/datasets/test_transforms.py
+++ tests/datasets/test_transforms.py
@@ -3,7 +3,7 @@ from datasets.dataset_torch import TorchDataset
 from datasets.transforms import transform_ts_features_to_dekadal, transform_stack_ts_static_features
 
 def test_transforms():
-    train_dataset = Dataset.load("test_softwheat_nl")
+    train_dataset = Dataset.load("maize_NL")
     train_dataset = TorchDataset(train_dataset)
     batch = [train_dataset[i] for i in range(16)]
     batch = TorchDataset.collate_fn(batch).copy()
